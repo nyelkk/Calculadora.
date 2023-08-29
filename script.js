8623b5d81abf835a036ba24085db75687d31271a
Liminha <123602429+nyelkk@users.noreply.github.com>
2023-08-29 08:40:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function insert(y){
+   var numero = document.getElementById('resultados').innerHTML
+   document.getElementById('resultados').innerHTML = numero + y
+}
+function limpar(){
+    document.getElementById('resultados').innerHTML = ""
+}
